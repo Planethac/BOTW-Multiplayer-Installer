0bf4ced08937082cb681495fff128fdb271beec2
Planethac <paul@rohrbach.cc>
2023-11-23 19:21:59 +0100

    Json Serilizer / DeSerilizer

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+namespace botwm
+{
+
+    public class Settings
+    {
+        public double installedVer { get; set; }
+    }
+    public class Setup
+    {
+        public static void Main(String[] args)
+        {
+            makeJson.settings();
+            Console.WriteLine("BOTW Multiplayer Setup");
+            if (File.Exists("settings.json"))
+            {
+                String configFile = "settings.json";
+                String SearializedJson = File.ReadAllText(configFile);
+                Settings settings = JsonSerializer.Deserialize<Settings>(SearializedJson);
+                if (settings.installedVer == null) settings.installedVer = -1;
+                if (!(settings.installedVer == -1)) Console.WriteLine($"Current Ver: {settings.installedVer}");
+            }
+            else
+            {
+                Console.WriteLine("Current Ver: NOT FOUND");
+            }
+        }
+    }
+    
+    public class makeJson
+    {
+        public static void settings()
+        {
+            var Jsettings = new Settings
+            {
+                installedVer = 1.1
+            };
+            String fName = "settings.json";
+            String json = JsonSerializer.Serialize<Settings>(Jsettings);
+            File.WriteAllText(fName, json);
+        }
+    }
+}
